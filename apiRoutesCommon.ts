dc3e7238360df48ef0eb735b96f73e3ab7b8001b
preitinger <peter.reitinger@gmail.com>
2024-01-16 17:53:45 +0100

    interface AccumulatedReq and AccumulatedResp

--- apiRoutesCommon.ts
+++ apiRoutesCommon.ts
@@ -3,3 +3,13 @@ export type ApiResp<MySuccessResp> = MySuccessResp | {
     type: 'error';
     error: string;
 }
+
+export interface AccumulatedReq {
+    type: 'AccumulatedReq';
+    requests: { type: string }[]
+}
+
+export type AccumulatedResp = {
+    type: 'success';
+    responses: any[];
+}
